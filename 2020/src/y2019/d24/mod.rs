99f911c3ead1d9841ce863877817b1ca035d654d
kezenator <github@kezenator.com>
2020-12-15 17:13:35 +1000

    2019 Day 24 part 2 2020 Day 15 initial solution (~ 5 seconds for part 2)

--- 2020/src/y2019/d24/mod.rs
+++ 2020/src/y2019/d24/mod.rs
@@ -1,99 +1,292 @@
 use std::collections::HashSet;
+use std::fmt::{Debug, Error, Formatter};
 use crate::support::*;
 
 const INPUT: &str = include_str!("input.txt");
+const EXAMPLE: &str = "....#\n#..#.\n#..##\n..#..\n#....";
 
-#[derive(PartialEq, Eq, Hash, Clone)]
-struct Bugs
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+struct Pos
 {
-    image: CharGrid,
+    level: i64,
+    x: i64,
+    y: i64,
 }
 
-impl Bugs
+impl Pos
 {
-    pub fn new() -> Self
+    pub fn new(level: i64, x: i64, y: i64) -> Self
     {
-        Bugs
-        {
-            image: CharGrid::new_from_input(INPUT, '.'),
-        }
+        Self { level, x, y }
     }
 
-    pub fn next(&self) -> Self
+    pub fn neighbours(&self, part_2: bool) -> Vec<Self>
     {
-        let mut image = self.image.clone();
-
-        for pos in image.all_points()
+        if part_2
         {
-            let num_bugs = Point::directions_4().iter()
-                .map(|&d| self.image.get_char(&(pos + d)))
-                .filter(|&c| c == '#')
+            let mut result = Vec::new();
+
+            for dir in Point::directions_4()
+            {
+                let point = Point::new(self.x, self.y) + dir;
+
+                if point.x == 2 && point.y == 2
+                {
+                    // Recurse into the lower level
+
+                    if dir.x == 1
+                    {
+                        for i in 0..5
+                        {
+                            result.push(Pos::new(self.level + 1, 0, i));
+                        }
+                    }
+                    else if dir.x == -1
+                    {
+                        for i in 0..5
+                        {
+                            result.push(Pos::new(self.level + 1, 4, i));
+                        }
+                    }
+                    else if dir.y == 1
+                    {
+                        for i in 0..5
+                        {
+                            result.push(Pos::new(self.level + 1, i, 0));
+                        }
+                    }
+                    else // if dir.y == -1
+                    {
+                        for i in 0..5
+                        {
+                            result.push(Pos::new(self.level + 1, i, 4));
+                        }
+                    }
+                }
+                else if point.x < 0
+                {
+                    result.push(Pos::new(self.level - 1, 1, 2));
+                }
+                else if point.x > 4
+                {
+                    result.push(Pos::new(self.level - 1, 3, 2));
+                }
+                else if point.y < 0
+                {
+                    result.push(Pos::new(self.level - 1, 2, 1));
+                }
+                else if point.y > 4
+                {
+                    result.push(Pos::new(self.level - 1, 2, 3));
+                }
+                else
+                {
+                    // Just a normal neighbour in this level
+                    result.push(Pos::new(self.level, point.x, point.y));
+                }
+            }
+
+            result
+        }
+        else // part_1
+        {
+            Point::directions_4().iter()
+                .map(|d| Pos::new(0, self.x + d.x, self.y + d.y))
+                .filter(|p| p.x >= 0 && p.x < 5 && p.y >= 0 && p.y < 5)
+                .collect()
+        }
+    }
+}
+
+struct Eris
+{
+    part_2: bool,
+    bugs: HashSet<Pos>,
+}
+
+impl Eris
+{
+    pub fn new(input: &str, part_2: bool) -> Self
+    {
+        let mut bugs = HashSet::new();
+
+        let image = CharGrid::new_from_input(input, '.');
+
+        for point in image.all_points()
+        {
+            if image.get_char(&point) == '#'
+            {
+                bugs.insert(Pos::new(0, point.x, point.y));
+            }
+        }
+
+        Eris { part_2, bugs }
+    }
+
+    pub fn evolve(self) -> Self
+    {
+        let mut new_bugs = HashSet::new();
+
+        for pos in self.all_positions()
+        {
+            let cur_bug = self.bugs.contains(&pos);
+            let neighbour_bugs = pos.neighbours(self.part_2).iter()
+                .filter(|p| self.bugs.contains(p))
                 .count();
 
-            if self.image.get_char(&pos) == '#'
+            let new_bug = if cur_bug && neighbour_bugs != 1
             {
-                if num_bugs != 1
-                {
-                    image.put_char(&pos, '.');
-                }
+                false
             }
-            else // cur empty
+            else if !cur_bug && (neighbour_bugs == 1 || neighbour_bugs == 2)
             {
-                if num_bugs == 1 || num_bugs == 2
-                {
-                    image.put_char(&pos, '#');
-                }
+                true
+            }
+            else
+            {
+                cur_bug
+            };
+
+            if new_bug
+            {
+                new_bugs.insert(pos);
             }
         }
 
-        Bugs { image }
+        Eris
+        {
+            part_2: self.part_2,
+            bugs: new_bugs,
+        }
     }
 
-    pub fn biodiversity(&self) -> u64
+    pub fn all_positions(&self) -> Vec<Pos>
     {
-        let mut result: u64 = 0;
-        let mut mask: u64 = 1;
+        let mut result = Vec::new();
 
-        for y in 0..self.image.get_height()
+        let level_range = if self.part_2
         {
-            for x in 0..self.image.get_width()
+            (self.min_level() - 1) .. (self.max_level() + 2)
+        }
+        else // part_1
+        {
+            0..1
+        };
+
+        for level in level_range
+        {
+            for y in 0..5
             {
-                if '#' == self.image.get_char(&Point::new(x, y))
+                for x in 0..5
                 {
-                    result |= mask;
+                    if !self.part_2 || x != 2 || y != 2
+                    {
+                        result.push(Pos::new(level, x, y));
+                    }
                 }
-                mask <<= 1;
             }
         }
 
         result
     }
+
+    pub fn biodiversity(&self) -> u64
+    {
+        assert!(!self.part_2);
+
+        self.bugs.iter()
+            .map(|pos| 1u64 << (pos.x + 5 * pos.y))
+            .sum()
+    }
+
+    pub fn num_bugs(&self) -> usize
+    {
+        self.bugs.len()
+    }
+
+    pub fn min_level(&self) -> i64
+    {
+        self.bugs.iter()
+            .map(|p| p.level)
+            .min()
+            .unwrap_or(0)
+    }
+
+    pub fn max_level(&self) -> i64
+    {
+        self.bugs.iter()
+            .map(|p| p.level)
+            .max()
+            .unwrap_or(0)
+    }
 }
 
-fn part_1() -> u64
+impl Debug for Eris
+{
+    fn fmt<'a>(&self, fmt: &mut Formatter<'a>) -> Result<(), Error>
+    {
+        for level in (self.min_level())..(self.max_level() + 1)
+        {
+            writeln!(fmt, "Level {}", level)?;
+            writeln!(fmt, "=====")?;
+
+            for y in 0..5
+            {
+                for x in 0..5
+                {
+                    if self.part_2 && x == 2 && y == 2
+                    {
+                        write!(fmt, "?")?;
+                    }
+                    else if self.bugs.contains(&Pos::new(0, x, y))
+                    {
+                        write!(fmt, "#")?;
+                    }
+                    else
+                    {
+                        write!(fmt, ".")?;
+                    }
+                }
+                writeln!(fmt)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+fn part_1(input: &str) -> u64
 {
     let mut seen = HashSet::new();
-    let mut state = Bugs::new();
+    let mut state = Eris::new(input, false);
 
     loop
     {
-        if !seen.insert(state.clone())
+        if !seen.insert(state.biodiversity())
         {
             return state.biodiversity();
         }
         
-        state = state.next();
+        state = state.evolve();
     }
 }
 
-fn part_2() -> String
+fn part_2(input: &str, minutes: usize) -> usize
 {
-    "not complete".to_owned()
+    let mut state = Eris::new(input, true);
+
+    for _ in 0..minutes
+    {
+        state = state.evolve();
+    }
+
+    state.num_bugs()
 }
 
 pub fn puzzles() -> PuzzleDay
 {
     puzzle_day(24)
-        .part_1(|| Answer { calculated: part_1(), expected: 17863741, })
-        .part_2(|| Answer { calculated: part_2(), expected: "not complete", })
+        .example(|| Answer{ calculated: part_1(EXAMPLE), expected: 2129920, })
+        .part_1(|| Answer { calculated: part_1(INPUT), expected: 17863741, })
+        .example(|| Answer{ calculated: part_2(EXAMPLE, 10), expected: 99, })
+        .part_2(|| Answer { calculated: part_2(INPUT, 200), expected: 2029, })
 }
---
+++ 2020/src/y2020/d15/mod.rs
@@ -0,0 +1,98 @@
+use std::collections::HashMap;
+use crate::support::*;
+
+const EXAMPLE_1: &str = "0,3,6";
+const INPUT: &str = "2,15,0,9,1,20";
+
+pub fn after_turns(input: &str, total_turns: usize) -> usize
+{
+    assert!(total_turns > 0);
+
+    let starting = input
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    if total_turns <= starting.len()
+    {
+        return starting[total_turns - 1];
+    }
+
+    let mut number_to_turn = HashMap::new();
+
+    for t in 0..starting.len()
+    {
+        let turn = t + 1;
+        let num = starting[t];
+
+        number_to_turn.insert(num, turn);
+    }
+
+    let mut turn = starting.len() + 1;
+    let mut last_spoken = starting[starting.len() - 1];
+
+    loop
+    {
+        let answer = match number_to_turn.get(&last_spoken)
+        {
+            Some(&last_turn_spoken) =>
+            {
+                turn - 1 - last_turn_spoken
+            },
+            None =>
+            {
+                0
+            },
+        };
+
+        number_to_turn.insert(last_spoken, turn - 1);
+
+        if turn == total_turns
+        {
+            return answer;
+        }
+
+        last_spoken = answer;
+        turn += 1;
+    }
+}
+
+pub fn part_1(input: &str) -> usize
+{
+    after_turns(input, 2020)
+}
+
+fn part_2(input: &str) -> usize
+{
+    after_turns(input, 30000000)
+}
+
+pub fn puzzles() -> PuzzleDay
+{
+    puzzle_day(15)
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 1), expected: 0, })
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 2), expected: 3, })
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 3), expected: 6, })
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 4), expected: 0, })
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 5), expected: 3, })
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 6), expected: 3, })
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 7), expected: 1, })
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 8), expected: 0, })
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 9), expected: 4, })
+        .example(|| Answer { calculated: after_turns(EXAMPLE_1, 10), expected: 0, })
+        .example(|| Answer { calculated: part_1("1,3,2"), expected: 1, })
+        .example(|| Answer { calculated: part_1("2,1,3"), expected: 10, })
+        .example(|| Answer { calculated: part_1("1,2,3"), expected: 27, })
+        .example(|| Answer { calculated: part_1("2,3,1"), expected: 78, })
+        .example(|| Answer { calculated: part_1("3,2,1"), expected: 438, })
+        .example(|| Answer { calculated: part_1("3,1,2"), expected: 1836, })
+        .part_1(|| Answer { calculated: part_1(INPUT), expected: 1280, })
+        .example(|| Answer { calculated: part_2("0,3,6"), expected: 175594, })
+        .example(|| Answer { calculated: part_2("1,3,2"), expected: 2578, })
+        .example(|| Answer { calculated: part_2("2,1,3"), expected: 3544142, })
+        .example(|| Answer { calculated: part_2("1,2,3"), expected: 261214, })
+        .example(|| Answer { calculated: part_2("2,3,1"), expected: 6895259, })
+        .example(|| Answer { calculated: part_2("3,2,1"), expected: 18, })
+        .example(|| Answer { calculated: part_2("3,1,2"), expected: 362, })
+        .part_2(|| Answer { calculated: part_2(INPUT), expected: 651639, })
+}
